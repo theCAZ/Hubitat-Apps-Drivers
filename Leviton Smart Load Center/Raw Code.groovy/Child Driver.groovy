150a0549455f6f5289cfff80a08ecddd9ca2dd4b
Jason D Thomas <jdthomas24@yahoo.com>
2026-05-19 19:28:50 -0500

    Update Child Driver.groovy

--- Leviton Smart Load Center/Raw Code.groovy/Child Driver.groovy
+++ Leviton Smart Load Center/Raw Code.groovy/Child Driver.groovy
@@ -35,7 +35,7 @@ metadata {
         namespace  : "jdthomas24",
         author     : "Community Port from rwoldberg/ldata-ha",
         description: "Child device for Leviton Smart Panel breaker or CT sensor",
-        version    : "1.1.8"
+        version    : "1.2.0"
     ) {
         // Core capabilities
         capability "Switch"           // on/off = breaker reset/trip
