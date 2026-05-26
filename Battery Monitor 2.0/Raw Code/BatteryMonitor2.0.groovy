019052052d586dd2a65aac617762bd84e120f7f1
Jason D Thomas <jdthomas24@yahoo.com>
2026-05-26 12:24:27 -0500

    Update BatteryMonitor2.0.groovy

--- Battery Monitor 2.0/Raw Code/BatteryMonitor2.0.groovy
+++ Battery Monitor 2.0/Raw Code/BatteryMonitor2.0.groovy
@@ -366,8 +366,11 @@ def mainPage() {
 
         section("<b>Diagnostics</b>") {
             input "debugMode", "bool", title: "Debug Logging (auto-disables after 30 min)", defaultValue: false, submitOnChange: true
-            paragraph "<span style='color:#94a3b8; font-size:11px;'>Battery Monitor v2.5.27</span>"
+            if (settings?.debugMode == true) {
+            runIn(1800, disableDebugLogging)
         }
+        paragraph "<span style='color:#94a3b8; font-size:11px;'>Battery Monitor v2.5.27</span>"
+        }    
     }
 }
 
